0d0d2e67d2fcd66c0af1128e5c0322934f83fdeb
zstone <zstonex@gmail.com>
2020-12-04 01:40:55 -0500

    getting new analysis packages

--- theories/ComplexBasics.v
+++ theories/ComplexBasics.v
@@ -1,7 +1,7 @@
 Require Import mathcomp.ssreflect.ssreflect.
 From mathcomp Require Import ssreflect ssrfun ssrbool ssrnat eqtype choice div.
 From mathcomp Require Import seq fintype bigop order ssralg ssrnum finmap matrix.
-From mathcomp Require Import topology normedtype derive all_real_closed.
+From mathcomp Require Import topology normedtype derive all_real_closed reals.
 Close Scope boolean_if_scope.
 Require Import Program.
 Set Implicit Arguments.
@@ -10,62 +10,39 @@ Unset Printing Implicit Defensive.
 Set Bullet Behavior "Strict Subproofs".
 
 Import Num.Theory GRing.Theory GRing.
-
-Section Holomorphic.
-Context {R : rcfType}.
-Notation C := R[i].
-Ltac as_R2 := 
-  match goal with 
-  | x : R[i] |- _ => destruct x 
-  end.
-Definition complex_lmodTypeR := Rcomplex_lmodType R.
-Definition r_pseudoMetric := (numFieldType_pseudoMetricType R).
-Definition r2_pseudoMetric := [pseudoMetricType R of (r_pseudoMetric * r_pseudoMetric)].
-
 Open Scope ring_scope.
 Open Scope complex_scope.
-Definition ball_R2 : C -> R -> C -> Prop := fun z eps z' => 
-  let: x +i* y := z in
-  let: x' +i* y' := z' in 
-  ball (R := R) (M := r2_pseudoMetric) 
-    (x,y) eps (x',y').
 
-Program Definition complex_PseudoMetric := 
-  @PseudoMetricMixin R C (entourage_ ball_R2) ball_R2 _ _ _ _.
-Next Obligation.
-  case: x; move => ??.
-  rewrite /ball_R2 /prod_ball.
-  by apply PseudoMetric.ax1.
-Qed.
-Next Obligation.
-  move: H; case: x; case: y => ????.
-  apply PseudoMetric.ax2.
-Qed.
-Next Obligation.
-  move: z y x H H0 => [r1 l1][r2 l2][r3 l3].
-  apply PseudoMetric.ax3.
-Qed.
-Program Definition complex_NormedModuleMixin := NormedModMixin (K := R) _.
+Reserved Notation "''Dz' f" (at level 0, f at level 0,
+  format "''Dz'  f").
+Section C_structures.
+Set Printing Coercions.
+Variable (R : realType).
+(* This is R[i], as seen as a 1-R[i] dimension normed vector space*)
+Definition C := numFieldType_normedModType [numFieldType of R[i]].
 
-Program Definition foo := @pseudoMetric_of_normedDomain R [normedZModType R of C] _.
+Section Holomorphic.
+Variable (V : normedModType C).
+Definition Holomorphic (f : C -> V) z := derivable f 1 z.
+Notation "''Dz' g" := ('D_1 g) .
+Check is_derive_cst.
 
-Program Definition complex_pseudoMetricType_mixin := topologyOfBaseMixin (T := C).
-  [uniformType of C].
+Definition g := (cst 0 : C -> V) .
+Lemma foo : forall q, 
+  g ^`() q = 0.
+Proof.
+  move => q //.
+  rewrite /g derive1E. 
+  have x (_ : (@is_derive_cst [numFieldType of derive1E]).
+  1: have x := (@diff_cst C) .
+  Search diff_cts.
+simpl.
 
-Print matrix_pseudoMetricType_mixin.
-Program Definition complex_pseudoMetricType_mixin := 
-  @PseudoMetricMixin R C.
-Local Open Scope ring_scope.
-Search (_ -> pseudoMetricType _).
-Definition complex_pseudoMetricType := 
-  
-Definition complex_pseudoMetricNormedZmodType := 
-  numFieldType_pseudoMetricNormedZmodMixin [numFieldType of R] .
+Definition CHolo (f : C -> V) z := 
+    Holomorphic f z /\
+    continuous ('Dz f) z .
 
-Canonical complex_pseudoMetricNormedZmodType.
-Definition foo := NormedModMixin (K := R) (V:=complex_pseudoMetricNormedZmodType).
-Check foo.
-Canonical structure (foo : pseudoMetricNormedZmodType C) := _.
-Canonical normedModType C. 
+End Holomorphic. 
 
-Definition Holomorphic (f : C -> V) F := differentiable f F.
+
+
