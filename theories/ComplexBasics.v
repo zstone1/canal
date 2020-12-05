9e5188425c368e0754b81dec66318a07eecebcc3
zstone <zstonex@gmail.com>
2020-12-05 01:13:16 -0500

    defining complex integration

--- theories/ComplexBasics.v
+++ theories/ComplexBasics.v
@@ -1,7 +1,9 @@
-Require Import mathcomp.ssreflect.ssreflect.
-From mathcomp Require Import ssreflect ssrfun ssrbool ssrnat eqtype choice div.
-From mathcomp Require Import seq fintype bigop order ssralg ssrnum finmap matrix.
-From mathcomp Require Import topology normedtype derive all_real_closed reals.
+From mathcomp Require Import ssreflect ssrfun ssrbool ssrnat eqtype choice div .
+From mathcomp Require Import seq finset fintype bigop order ssralg ssrnum finmap matrix interval.
+From mathcomp Require Import all_real_closed.
+From mathcomp Require Import 
+  boolp reals nngnum posnum classical_sets topology 
+  normedtype prodnormedzmodule derive reals landau forms.
 Close Scope boolean_if_scope.
 Require Import Program.
 Set Implicit Arguments.
@@ -9,40 +11,68 @@ Unset Strict Implicit.
 Unset Printing Implicit Defensive.
 Set Bullet Behavior "Strict Subproofs".
 
-Import Num.Theory GRing.Theory GRing.
-Open Scope ring_scope.
+Import Order.TTheory GRing.Theory Num.Theory.
 Open Scope complex_scope.
+Open Scope classical_set_scope.
+Open Scope ring_scope.
 
-Reserved Notation "''Dz' f" (at level 0, f at level 0,
-  format "''Dz'  f").
+Section C1.
+Context {R:numFieldType} {V : normedModType R}.
+
+Open Scope type_scope.
+Definition C_1 (f : R^o -> V^o) := forall (z:R),
+  { near (z:R^o), forall x, differentiable f x} /\
+  { near (z:R^o), continuous (f^`() : R^o -> _)}.
+End C1.
+
+Section SubspaceContinuity.
+Variable (X Y : topologicalType).
+
+Definition continuous_on (U : set X) (f: X^o -> Y) := 
+  (forall x, f @ (within U [filter of x]) --> f x).
+
+End SubspaceContinuity.
 Section C_structures.
 Set Printing Coercions.
 Variable (R : realType).
 (* This is R[i], as seen as a 1-R[i] dimension normed vector space*)
-Definition C := numFieldType_normedModType [numFieldType of R[i]].
+Definition C := R[i].
+Definition as_R2 (z:C) := let: x +i* y := z in (x,y).
 
 Section Holomorphic.
-Variable (V : normedModType C).
-Definition Holomorphic (f : C -> V) z := derivable f 1 z.
-Notation "''Dz' g" := ('D_1 g) .
-Check is_derive_cst.
+Context {V : normedModType C}.
 
-Definition g := (cst 0 : C -> V) .
-Lemma foo : forall q, 
-  g ^`() q = 0.
-Proof.
-  move => q //.
-  rewrite /g derive1E. 
-  have x (_ : (@is_derive_cst [numFieldType of derive1E]).
-  1: have x := (@diff_cst C) .
-  Search diff_cts.
-simpl.
-
-Definition CHolo (f : C -> V) z := 
-    Holomorphic f z /\
-    continuous ('Dz f) z .
-
+Definition Holomorphic_on U {O:open U} (f : C^o -> V) :=
+    {in U, forall z, derivable f 1 z}.
+Program Definition CHolo_on U {O:open U} (f : C^o -> V) (z:C^o) := 
+  Holomorphic_on (O:=O) f /\
+  {in U, continuous (f^`() : C^o -> _) }.
 End Holomorphic. 
+
+Section ComplexIntegrals.
+
+Context {V : normedModType R}.
+
+Record CIntegral := {
+  path : R^o -> C^o;
+  l_end : R^o;
+  r_end : R^o;
+  f : C^o -> V^o;
+
+  lltr : l_end < r_end;
+  cts_f : continuous_on 
+    (path @` itv_decompose `[l_end,r_end]) f;
+  (* continuously differentiable on the open interval*)
+  pathdiff : {in `]l_end,r_end[, C_1 
+    (as_R2 \o path : R^o -> R^o * R^o)  };
+  (* continuous on the closed interval*)
+  pathcts : continuous_on (X:=[topologicalType of R^o])
+    (itv_decompose `[l_end, r_end]) path;
+}.
+
+Section 
+
+
 
 
 
